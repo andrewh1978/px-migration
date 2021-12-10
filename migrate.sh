df2baaa5225b31fafe8853dfb12729572721ca52
Andrew Hill <andrew@portworx.com>
2021-12-10 17:09:21 +0000

    rewrite

--- migrate.sh
+++ migrate.sh
@@ -1,58 +1,142 @@
 NAMESPACE=postgres
-STORAGECLASS=portworx-sc
-SUFFIX=px
+LABEL=px/migrate=true
 
-JQ="docker run -i --rm --name jq endeveit/docker-jq jq"
+WORKDIR=/var/tmp/px-migration
+rm -rf $WORKDIR
+mkdir -p $WORKDIR/{pvc,pv-old,pv-new}
 
-# FIXME get original number of replicas for each deployment/statefulset
+function pxctl {
+  ns=$(kubectl get pod -lname=portworx -A -o jsonpath='{.items[].metadata.namespace}')
+  pod=$(kubectl get pod -lname=portworx -A -o jsonpath='{.items[].metadata.name}')
+  kubectl exec -n $ns -c portworx $pod -- /opt/pwx/bin/pxctl --color $*
+}
 
-# Scale deployments and statefulsets to zero
-kubectl scale deployments --all --replicas 0 -n $NAMESPACE
-kubectl scale sts --all --replicas 0 -n $NAMESPACE
+
+IFS='' read -r -d '' jobtemplate <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: migrate-job-script
+  namespace: NAMESPACE
+data:
+  entrypoint.sh: |-
+    #!/bin/sh
+    apk add rsync
+    rsync -a /migrate-old/ /migrate-new
+    df -h
+---
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: migrate-job
+  namespace: NAMESPACE
+spec:
+  template:
+    spec:
+      terminationGracePeriodSeconds: 0
+      restartPolicy: Never
+      containers:
+      - name: migrate-job
+        image: alpine:3.15.0
+        command:
+        - /entrypoint.sh
+        volumeMounts:
+        - name: migrate-job-script
+          mountPath: /entrypoint.sh
+          readOnly: true
+          subPath: entrypoint.sh
+        - name: migrate-old
+          mountPath: /migrate-old
+        - name: migrate-new
+          mountPath: /migrate-new
+      volumes:
+      - name: migrate-old
+        persistentVolumeClaim:
+          claimName: PVC
+      - name: migrate-new
+        persistentVolumeClaim:
+          claimName: px-migration-temp
+      - name: migrate-job-script
+        configMap:
+          defaultMode: 0700
+          name: migrate-job-script
+EOF
+
+# FIXME this is for testing
+kubectl scale deploy --all -n $NAMESPACE --replicas 0
+kubectl label pvc --all --overwrite -n $NAMESPACE $LABEL
+
+# Create Portworx StorageClass
+kubectl apply -f - <<EOF
+kind: StorageClass
+apiVersion: storage.k8s.io/v1
+metadata:
+  name: portworx-sc
+provisioner: kubernetes.io/portworx-volume
+parameters:
+  repl: "1"
+allowVolumeExpansion: true
+reclaimPolicy: Retain
+EOF
 
 # Get list of PVCs
-pvcs_sts=$(kubectl get sts -n $NAMESPACE -o jsonpath='{.items[*].spec.volumeClaimTemplates[*].metadata.name}' 2>/dev/null)
-pvcs_deploy=$(kubectl get deploy -n $NAMESPACE -o jsonpath='{.items[*].spec.template.spec.volumes[*].persistentVolumeClaim.claimName}' 2>/dev/null)
-PVCs="$pvcs_sts $pvcs_deploy"
+PVCs=$(kubectl get pvc -l $LABEL -n $NAMESPACE -o jsonpath='{.items[*].metadata.name}')
 
-# Get list of deployments
-DEPLOYMENTS=$(kubectl get deploy -n $NAMESPACE -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
+# Get list of PVs
+PVs=$(kubectl get pvc -l $LABEL -n $NAMESPACE -o jsonpath='{.items[*].spec.volumeName}')
 
-# Get list of statefulsets
-STATEFULSETS=$(kubectl get sts -n $NAMESPACE -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
-
-# Create new PVCs
-for pvc in $PVCs; do
-  # FIXME also get accessmodes and labels
-  size=$(kubectl get pvc $pvc -n $NAMESPACE -o jsonpath='{.spec.resources.requests.storage}')
-  cat portworx-pvc.yml.template |
-    sed s/STORAGECLASS/$STORAGECLASS/ |
-    sed s/NAMESPACE/$NAMESPACE/ |
-    sed s/PVC/$pvc-$SUFFIX/ |
-    sed s/SIZE/$size/ |
-    kubectl apply -f -
+# Patch PVs so they are not deleted
+for pv in $PVs; do
+  kubectl patch pv $pv -p '{"spec":{"persistentVolumeReclaimPolicy":"Retain"}}'
 done
 
-# Run migration jobs
 for pvc in $PVCs; do
-  cat migrate-job.yml.template |
+  # Get PVC size
+  size=$(kubectl get pvc $pvc -n $NAMESPACE -o jsonpath='{.spec.resources.requests.storage}')
+  # Create temp PVC
+  kubectl apply -n $NAMESPACE -f - <<EOF
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+   name: px-migration-temp
+   annotations:
+     volume.beta.kubernetes.io/storage-class: portworx-sc
+spec:
+   accessModes:
+     - ReadWriteOnce
+   resources:
+     requests:
+       storage: $size
+EOF
+  kubectl patch pvc px-migration-temp -n $NAMESPACE -p '{"spec":{"resources":{"requests":{"storage":"'$size'"}}}}'
+  # Copy data from old PVC to temp PVC
+  cat <<<"$jobtemplate" |
     sed s/NAMESPACE/$NAMESPACE/ |
     sed s/PVC/$pvc/ |
-    sed s/SUFFIX/$SUFFIX/ |
     kubectl apply -f -
   kubectl wait --for=condition=complete --timeout=86400s job/migrate-job -n $NAMESPACE
   kubectl delete job migrate-job -n $NAMESPACE
+  # Delete but save old PVC
+  oldpv=$(kubectl get pvc $pvc -n $NAMESPACE -o jsonpath='{.spec.volumeName}')
+  kubectl get pvc $pvc -n $NAMESPACE -o yaml >$WORKDIR/pvc/$pvc.yml
+  kubectl delete pvc $pvc -n $NAMESPACE
+  # Delete temp PVC
+  newpv=$(kubectl get pvc px-migration-temp -n $NAMESPACE -o jsonpath='{.spec.volumeName}')
+  kubectl delete pvc px-migration-temp -n $NAMESPACE
+  # Delete but save old PV
+  kubectl get pv $oldpv -o yaml >$WORKDIR/pv-old/$oldpv.yml
+  kubectl delete pv $oldpv
+  # Rename Portworx volume
+  pxctl volume clone --name $oldpv $newpv
+  pxctl volume delete $newpv -f
+  # Rename temp PV to old PV
+  kubectl get pv $newpv -o yaml | sed "s/^  name: .*/  name: $oldpv/;s/^    volumeID.*/    volumeID: $oldpv/" >$WORKDIR/pv-new/$oldpv.yml
+  kubectl delete pv $newpv
+  kubectl apply -f $WORKDIR/pv-new/$oldpv.yml
+  kubectl patch pv $oldpv --type=json -p="[{'op': 'remove', 'path': '/spec/claimRef'}]"
+  # Apply old PVC
+  kubectl apply -f $WORKDIR/pvc/$pvc.yml
 done
 
-# Patch deployments for new PVCs
-for deployment in $DEPLOYMENTS; do
-  # Get number of PVCs
-  n=$(kubectl get deploy $deployment -n $NAMESPACE -o json | $JQ -r '.spec.template.spec.volumes[].name' | wc -l)
-  for i in $(seq 1 $n); do
-    # Get PVC name
-    pvc=$(kubectl get deploy $deployment -n $NAMESPACE -o jsonpath='{.spec.template.spec.volumes[0].persistentVolumeClaim.claimName}')
-    kubectl patch deploy $deployment -n $NAMESPACE --type='json' -p '[{"op":"replace","path":"/spec/template/spec/volumes/'$[$i-1]'/persistentVolumeClaim/claimName","value":"'$pvc-$SUFFIX'"}]'
-  done
-done
-
-# FIXME repeat for statefulsets
+# Clean up
+kubectl delete sc portworx-sc
